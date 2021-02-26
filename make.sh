88ed9e25a834e44e668f7ec1d9d87da54c15240a
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-02-26 15:46:04 -0800

    loop

--- make.sh
+++ make.sh
@@ -1,5 +1,9 @@
-git fetch --all
-git reset --hard origin/main
-bash index.sh > index.html
-cd ..
-firebase deploy
+while true
+do
+	git fetch --all
+	git reset --hard origin/main
+	bash index.sh > index.html
+	cd ..
+	firebase deploy
+	sleep 300
+done
