aabd476ef5cb138f870200f12015d207b1082750
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-02-27 00:49:35 -0800

    yui minify

--- make.sh
+++ make.sh
@@ -4,6 +4,8 @@ do
 	git fetch --all
 	git reset --hard origin/main
 	bash index.sh > public/index.html
+	yui-compressor style.css > style.css
+	yui-compressor script.js > script.js
 	firebase deploy
 	sleep ${duration}
 done
