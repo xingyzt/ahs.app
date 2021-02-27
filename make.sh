8811895de0e231916202afa7da724033a9cb24eb
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-02-27 01:05:22 -0800

    style path

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ do
 	git fetch --all
 	git reset --hard origin/main
 	bash index.sh > public/index.html
-	yui-compressor style.css -o style.css
+	yui-compressor public/style.css -o public/style.css
 	firebase deploy
 	sleep ${duration}
 done
