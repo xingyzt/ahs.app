f2d1bd13853f4673e9a8de1c39f38680d6be980a
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-02-28 12:29:32 -0800

    Compress js with terser

--- make.sh
+++ make.sh
@@ -3,8 +3,11 @@ while true
 do
 	git fetch --all
 	git reset --hard origin/main
-	bash index.sh > public/index.html
-	yui-compressor public/style.css -o public/style.css
+
+	bash bin/index.sh > public/index.html
+	yui-compressor bin/style.css -o public/style.css
+	terser --compress --mangle --toplevel bin/script.js -o public/script.js
+
 	firebase deploy
 	sleep ${duration}
 done
