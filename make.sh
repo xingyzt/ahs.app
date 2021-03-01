fe21a2a8905798e17359d395951fe0c60ce23cfe
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-03-01 13:04:42 -0800

    remove while loop

--- make.sh
+++ make.sh
@@ -1,13 +1,6 @@
-duration=$1
-while true
-do
-	cd src
-		bash index.sh > ../dist/index.html
-		yui-compressor style.css -o ../dist/style.css
-		terser --compress --mangle --toplevel script.js -o ../dist/script.js
-		cp icon.png ../dist/icon.png
-	cd ..
-
-	firebase deploy
-	sleep ${duration}
-done
+cd src
+	bash index.sh	> ../dist/index.html
+	cp style.css	> ../dist/style.css
+	cp script.js	> ../dist/script.js
+	cp icon.png ../dist/icon.png
+cd ..
