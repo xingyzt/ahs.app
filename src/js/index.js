72baaa128ab50af5f066871ed05a7ff1dd0c6047
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-03-09 21:06:57 +0000

    extract write_article

--- src/js/index.js
+++ src/js/index.js
@@ -37,30 +37,7 @@ async function show_article() {
 	if (!article) return false
 
 	document.title = article.title
-
-	Article.querySelector('h2').focus()
-	for (const property in article) {
-		const element = Article.querySelector('.' + property)
-		if (!element) continue
-		element.innerHTML = article[property]
-	}
-	
-	const Media = Article.querySelector('.media')
-	const media_cache = []
-	if(article.videoIDs) for (const id of article.videoIDs){
-		const embed = clone_template('youtube')
-		embed.src = embed.src.replace('[URL]',id)
-		media_cache.push(embed)
-		embed.addEventListener('load',safe_center)
-	}
-	if(article.imageURLs) for (const url of article.imageURLs){
-		const image = clone_template('image')
-		image.src = url
-		media_cache.push(image)
-		image.addEventListener('load',safe_center)
-	}
-	Media.style.alignContent = 'safe center'
-	Media.replaceChildren(...media_cache)
+	write_article(Article,article)
 }
 async function safe_center(){
 	Media.style.alignContent = Media.scrollWidth > window.innerWidth ? 'flex-start' : 'safe center'
