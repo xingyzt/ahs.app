07e7baf2dff797f7b3c1aafcad59d62888689c44
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-04-02 12:36:01 -0700

    Add back 'built site' printout

--- make.sh
+++ make.sh
@@ -1,11 +1,28 @@
-mkdir -p dist
+#!/bin/bash
+
+. .env
 
 cd src
-	bash index.sh >	../dist/index.html
-	cat css/* > ../dist/index.css
-	cat js/* > ../dist/index.js
-	cp media/* ../dist/
+
+html=$(cat index.html)
+js=$(cat script.js)
+css=$(cat style.css)
+
+auth=$(curl "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$KEY" \
+-H "Content-Type: application/json" \
+--data-binary "{\"email\":\"$EMAIL\",\"password\":\"$PASSWORD\",\"returnSecureToken\":true}")
+
+access_token=$(jq -rc '.idToken' <<< $auth)
+host="https://ahs-app.firebaseio.com"
+curl -Z "$host/{locationIDs,locations,categories,snippets,week,schedules}.json?auth=$access_token" -o "/tmp/#1.json"
+
+snippets=$(jq -sfr snippets.jq /tmp/locationIDs.json /tmp/locations.json /tmp/categories.json /tmp/snippets.json)
+schedule=$(jq -sfr schedule.jq /tmp/week.json /tmp/schedules.json)
+
+time=$(TZ=":America/Los_Angeles" date +"%l:%M %P Pacific Time")
+
 cd ..
 
-echo
-echo built site
+printf "${html}" "${css}" "${schedule}" "${snippets}" "${time}" "${js}" > dist/index.html
+
+echo "built site"
--- server.py
+++ server.py
@@ -9,11 +9,11 @@ import socketserver
 import re
 from pathlib import Path
 import os
+import sys
 
 os.chdir(os.path.join(os.path.dirname(__file__),'dist'))
-
-HOST = ('0.0.0.0', 8000)
-pattern = re.compile('.png|.jpg|.jpeg|.js|.css|.ico|.gif|.svg', re.IGNORECASE)
+port = int(sys.argv[1]) or 8000
+HOST = ('0.0.0.0', port)
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
@@ -21,12 +21,12 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         url_parts = urllib.parse.urlparse(self.path)
         request_file_path = Path(url_parts.path.strip("/"))
 
-        ext = request_file_path.suffix
-        if not request_file_path.is_file() and not pattern.match(ext):
+        if not request_file_path.is_file():
             self.path = 'index.html'
 
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 
 httpd = socketserver.TCPServer(HOST, Handler)
+print('Serving ahs.app at http://localhost:'+str(port))
 httpd.serve_forever()
