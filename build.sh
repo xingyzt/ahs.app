9399bbac1d86e7b95fb8e329c95650cdf48258b3
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2021-06-03 16:23:08 +0000

    reorg: use stylesheet & script files

--- build.sh
+++ build.sh
@@ -4,20 +4,24 @@ export TZ=US/Pacific date
 
 cd src
 
-html=$(cat index.html)
-js=$(cat script.js)
-css=$(cat style.css)
+# merge scripts and styles into single files
+cat src/css/* > dist/style.css
+cat src/js/* > dist/script.js
+
+# copy static assets
+cp src/static/* dist/
+
+# insert data into HTML
+html=$(cat src/index.html)
 
 curl -ZL "db.ahs.app/{locationIDs,locations,categories,snippets,weekID,weeks,schedules}.json" -o "/tmp/#1.json"
 
-snippets=$(jq -sfr snippets.jq /tmp/locationIDs.json /tmp/locations.json /tmp/categories.json /tmp/snippets.json)
-schedule=$(jq -sfr schedule.jq /tmp/weekID.json /tmp/weeks.json /tmp/schedules.json)
+snippets=$(jq -sfr src/jq/snippets.jq /tmp/locationIDs.json /tmp/locations.json /tmp/categories.json /tmp/snippets.json)
+schedule=$(jq -sfr src/jq/schedule.jq /tmp/weekID.json /tmp/weeks.json /tmp/schedules.json)
 
 time=$(date +"%l:%M %P Pacific Time")
 
-cd ..
-
-printf "$html" "$css" "$schedule" "$snippets" "$time" "$js" > dist/index.html
+printf "$html" "$schedule" "$snippets" "$time"> dist/index.html
 
 echo "
 built site"
