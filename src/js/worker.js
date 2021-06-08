b16d0c80ff34955e6457701a75f387e3432ae1da
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-06-08 16:13:56 -0700

    Fix response type; Timeout math

--- src/js/worker.js
+++ src/js/worker.js
@@ -9,25 +9,22 @@ const response = async request => {
 	const cache = await caches.open(version)
 	const cachedResponse = await cache.match(request)
 
-	let cacheIsValid = true
-
+	const now = Date.now()
+	const second = 1000
+	const minute = 60 * second
+	const hour = 60 * minute
+	const timeout = request.url.endsWith('.json') ? 10 * minute : 24 * hour
+	
 	const headerName = 'ahs-app-last-fetched'
 
-	if (request.url.endsWith('.json')) {
-		const now = Date.now()
-		const minute = 60 * 1000
-		const timeout = 10 * minute
-		cacheIsValid = parseInt(cachedResponse.headers.get(headerName)) - now < timeout)
-	}
-	
-	if (cachedResponse && cacheIsValid) return cachedResponse
+	if (cachedResponse && now - parseInt(cachedResponse.headers.get(headerName)) < timeout)
+		return cachedResponse
 	
 	const freshResponse = await fetch(request)
 	const clonedResponse = freshResponse.clone()
 	const headers = new Headers(clonedResponse.headers)
-	headers.append(headerName,now)
-	const body = await clonedResponse.json()
-	cache.put(request, new Response(body, { headers }))
+	headers.append(headerName,now.toString())
+	cache.put(request, new Response(clonedResponse.body, { headers }))
 	return freshResponse
 
 }
