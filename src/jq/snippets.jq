920510cacfdffc5f5296463e54dc584292a5b4b7
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-06-10 22:03:53 -0700

    Use separate request/response pair for timestamps

--- src/jq/snippets.jq
+++ src/jq/snippets.jq
@@ -2,8 +2,8 @@ def n: join("\n");
 def slug: gsub("[^\\w\\d]+";"-") | gsub("^-|-$";"");
 def rot13: explode | map(
 	if 65 <= . and . <= 90 then ((. - 52) % 26) + 65
-        elif 97 <= . and . <= 122 then (. - 84) % 26 + 97
-        else . end
+	elif 97 <= . and . <= 122 then (. - 84) % 26 + 97
+	else . end
 ) | implode;
 
 .[0] as $locationIDs
--- src/js/worker.js
+++ src/js/worker.js
@@ -2,7 +2,7 @@ self.addEventListener('fetch', event => event.respondWith(response(event.request
 self.addEventListener('install', event => event.waitUntil(self.skipWaiting()))
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()))
 
-const version = 5
+const version = 6
 
 const response = async request => {
 
@@ -12,25 +12,39 @@ const response = async request => {
 	const now = Date.now()
 	const second = 1000
 	const minute = 60 * second
-	const timeout = 10 * minute
-	const header_name = 'ahs-app-last-fetched'
+	const hour = 60 * minute
+	const day = 24 * hour
+	const year = 365 * day
 
-	const forever = !['application/json','text/html'].includes(request.headers.get('Content-Type'))
+	const timeouts = {
+		'application/json': 10 * minute,
+		'text/html': 10 * minute,
+		'text/css': day,
+		'application/javascript': day,
+		'font/ttf': year,
+		'image/jpg': year,
+		'image/jpeg': year,
+		'image/png': year,
+	}
 
-	if ( cached_response &&
-		( forever || !navigator.onLine || (now - parseInt(cached_response.headers.get(header_name)) < timeout) )
-	) return cached_response
+	const cached_time_url = request.url + '?cache_time'
+	let cached_time_response = await cache.match(cached_time_url)
+	const cached_time = cached_time_response ? parseInt(await cached_time_response.text()) : 0
+	cached_time_response = new Response(now.toString())
+
+	if ( cached_response ) {
+		const content_type = `${cached_response.headers.get('content-type')}`.split(';')[0]
+		const timeout = timeouts[content_type] || day
+		console.log({content_type,timeout})
+		if( !navigator.onLine || now - cached_time < timeout )
+			return cached_response
+	}
 	
 	const fresh_response = await fetch(request)
 	const cloned_response = fresh_response.clone()
 
-	if (forever) {
-		cache.put(request, cloned_response)
-	} else {
-		const headers = new Headers(cloned_response.headers)
-		headers.append(header_name, now.toString())
-		cache.put(request, new Response(cloned_response.body, { headers }))
-	}
+	cache.put(request.url, cloned_response)
+	cache.put(cached_time_url, cached_time_response)
 	return fresh_response
 
 }
