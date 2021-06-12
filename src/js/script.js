b5939b92b5afcfec1309f365444f4c329df8aac9
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-06-12 13:20:59 -0700

    Use color attribute for loading images

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 main()
 
 async function main() {
@@ -28,10 +27,10 @@ async function show_article() {
 	$main.replaceChild($article,$main.firstChild)
 	window.scrollTo(0,0)
 
-	$main.hidden = window.location.pathname==='/'
+	$main.hidden = location.pathname==='/'
 	if($main.hidden) return reset_title()
 
-	const id = rot13(window.location.pathname.split('/').pop()) // Last portion of the path is the ciphered ID
+	const id = rot13(location.pathname.split('/').pop())
 
 	const article = await db('articles/'+id)
 	if (!article) return reset_title()
@@ -42,6 +41,7 @@ async function show_article() {
 		const element = $article.querySelector('.' + property)
 		if (element) element.innerHTML = article[property]
 	}
+	// $article.style.setProperty('--color',article.color)
 	
 	const $media = $article.querySelector('.media')
 	$media.style.alignContent = 'safe center'
@@ -58,12 +58,72 @@ async function show_article() {
 			return $image
 		}))),
 	)
-	/*
-	const $related = $article.querySelector('.related')
-	$related.append(
-		...await Promise.all(( article.relatedArticleIDs || [] ).map( async id => {
-			const snippet = await db('snippets',id)
-			const $snippet = clone_template('snippet')
+	return true
+}
+async function safe_center() {
+	const $media = document.querySelector('main>.article>.media')
+	$media.style.alignContent = $media.scrollWidth > window.innerWidth ? 'flex-start' : 'safe center'
+}
+async function db(...path) {
+	const response = await fetch(
+		`https://ahs-app.firebaseio.com/${path.join('/')}.json`,
+		{ headers: { 'Content-Type': 'application/json' } },
+	)
+	return await response.json()
+}
+async function highlight_schedule($cell) {
+	const $schedule = document.querySelector('.schedule')
+	if(!$schedule) return
 
-			$snippet.href = '/' + 
+	const class_name = 'highlighted-period'
+
+	const date = new Date()
+	const minutes = date.getHours()*60 + date.getMinutes()
+	const seconds = minutes*60 + date.getSeconds()
+
+	if(!$cell)
+		$cell = Array.from($schedule.querySelectorAll('td'))
+			.reverse()
+			.find(x=>parseInt(x.id)<=minutes)
+
+	if($cell) {
+		$cell.classList.add(class_name)
+		const Prev = $cell.previousElementSibling
+		if(Prev) Prev.classList.remove(class_name)
+	}
+
+	const $next = $cell.nextElementSibling
+	if($next) setTimeout(
+		highlight_schedule,
+		($next.id*60 - seconds)*1000,
+		$next
+	)
+}
+async function internal_link(event) {
+	history.pushState({}, '', event.target.href)
+	show_article()
+	document.activeElement.blur()
+	event.preventDefault()
+}
+async function match_color(image) {
+	image.addEventListener('load', () => {
+		$canvas.ctx.drawImage(image, 0, 0, 1, 1)
+		const data = $canvas.ctx.getImageData(0, 0, 1, 1).data
+		image.parentElement.style.setProperty('--color',`rgb(${data[0]}, ${data[1]}, ${data[2]})`)
+	})
+}
+function clone_template(name) {
+	return document.querySelector('.template-' + name)
+		.content.cloneNode(true)
+		.querySelector('*')
+} 
+function rot13(str) {
+	return str.replace( /[a-z]/gi, c =>
+		'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'[
+			'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.indexOf(c)
+		]
+	)
+}
+function slug(title) {
+	return title.replace(/[^\w\d]+/g,'-')
 }
