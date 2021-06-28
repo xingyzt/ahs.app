ec4b29403e0ab48e1c51eb03644cb5a2fa88c272
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-06-28 14:42:13 -0700

    Add option to build without downloading; Only set TZ temporarily

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export TZ=US/Pacific date
-
 mkdir -p dist
 
 # merge styles into single file
@@ -16,14 +14,12 @@ cp -RT src/static dist
 # insert data into HTML
 html=$(cat src/index.html)
 
-curl -ZL "db.ahs.app/{locationIDs,locations,categories,snippets,weekIDs,weeks,scheduleIDs,schedules}.json" -o "/tmp/#1.json"
-
 snippets=$(jq -sfr src/jq/snippets.jq /tmp/locationIDs.json /tmp/locations.json /tmp/categories.json /tmp/snippets.json)
 schedule=$(jq -sfr src/jq/schedule.jq /tmp/weekIDs.json /tmp/weeks.json /tmp/schedules.json)
 schedules=$(jq -sfr src/jq/schedules.jq /tmp/scheduleIDs.json /tmp/schedules.json)
 calendar=$(jq -sfr src/jq/calendar.jq /tmp/weekIDs.json /tmp/weeks.json /tmp/schedules.json)
 
-time=$(date +"%l:%M %P Pacific Time")
+time=$(TZ="US/Pacific date date" date +"%l:%M %P Pacific Time")
 
 printf "$html" "$schedule" "$snippets" "$schedules" "$calendar" "$time" > dist/index.html
 
--- makefile
+++ makefile
@@ -1,10 +1,20 @@
-build:
+build: download cache-build
+	:
+
+build-cache:
 	sh build.sh
+
+download:
+	curl -ZL "db.ahs.app/{locationIDs,locations,categories,snippets,weekIDs,weeks,scheduleIDs,schedules}.json" -o "/tmp/#1.json"
+
 archives:
 	sh archives.sh
+
 deploy: build
 	cp -RT dist /var/www/ahs.app
+
 host: build
 	python3 server.py 8000
+
 update:
 	git pull
