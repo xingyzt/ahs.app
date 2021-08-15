d9644aac3348310bf6215cd7827ac46960b87411
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2021-08-15 02:09:20 +0000

    fix some color issues

--- src/js/worker.js
+++ src/js/worker.js
@@ -1,4 +1,4 @@
-const version = 'dodecagon'
+const version = 'mammoth'
 
 self.addEventListener('fetch', event => event.respondWith(response(event.request)))
 self.addEventListener('install', event => event.waitUntil(self.skipWaiting()))
